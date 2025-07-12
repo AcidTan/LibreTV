f9ae2dc3b95ffc7892055cbdbc0fa6287bd1fb8c
AcidTan <2311818459@qq.com>
2025-07-12 20:47:53 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,9 +22,9 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-    gZapp: {
-        api: 'https://api.9cec79d.com',
-        name: '瓜子APP',
+    jszy: {
+        api: 'https://jszyapi.com/api.php/provide/vod',
+        name: '极速资源',
     },
     dyttzy: {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
